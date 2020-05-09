8517b67ff5a779f8ccc91c58b24abd3919dda240
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-05-09 16:51:02 +0800

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-  ],
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    semi: ['error', 'always'],
-    'space-before-function-paren': ['error', {
-      anonymous: 'always',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
-  },
+  extends: ["eslint:recommended"],
 };
