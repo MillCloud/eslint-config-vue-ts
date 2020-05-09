7eac04655c7f8081640945d147415e84481c3de1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-05-09 16:51:51 +0800

    chore: update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  globals: {
-    uni: "readonly",
-    plus: "readonly",
-  },
-  plugins: ["@typescript-eslint"],
+  parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
     extraFileExtensions: [".vue"],
@@ -17,64 +9,79 @@ module.exports = {
       jsx: true,
     },
   },
-  parser: "vue-eslint-parser",
+  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:vue/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
   ],
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    mocha: true,
+    node: true,
+  },
+  globals: {
+    uni: "readonly",
+    plus: "readonly",
+  },
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
+    // for projects developing
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // override eslint-config-standard and follow prettier default options
+    "comma-dangle": ["error", "always-multiline"],
+    // for projects developing
     "no-console":
       process.env.NODE_ENV === "production" ||
       process.env.NODE_ENV === "pre-production" ||
       process.env.NODE_ENV === "staging"
         ? "warn"
         : "off",
+    // for projects developing
     "no-debugger":
       process.env.NODE_ENV === "production" ||
       process.env.NODE_ENV === "pre-production" ||
       process.env.NODE_ENV === "staging"
         ? "warn"
         : "off",
-    "no-unused-vars":
+    // for projects developing
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars":
       process.env.NODE_ENV === "production" ||
       process.env.NODE_ENV === "pre-production" ||
       process.env.NODE_ENV === "staging"
         ? "warn"
         : "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
+    // override eslint-config-standard and follow prettier default options
+    quotes: "off",
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      { allowTemplateLiterals: false },
+    ],
+    // override eslint-config-standard and follow prettier default options
+    semi: "off",
     "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/space-before-function-paren": ["error", {
-      anonymous: 'always',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
+    // override eslint-config-standard and follow prettier default options
+    "space-before-function-paren": "off",
+    "@typescript-eslint/space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
   },
   overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {
-        "no-unused-vars": "off",
-      },
-    },
     {
       files: ["shims-tsx.d.ts"],
       rules: {
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
-      },
-    },
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-        mocha: true,
       },
     },
   ],
